bf62ca1007142e1ca65c6ae2d635280fabadfc2a
Jeff Sittler <mindblender@gmail.com>
2023-04-19 19:23:02 -0700

    WIP

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,12 @@
-import { defineConfig, loadEnv } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ command, mode }) => {
-  const env = loadEnv(mode, process.cwd(), "");
+  const env = loadEnv(mode, process.cwd(), '');
   return {
     plugins: [react()],
-    base: env.VITE_BASE ?? "/",
+    base: env.VITE_BASE ?? '/',
   };
 });
+
